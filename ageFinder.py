7834dbf10b20f459d37e15487c48e808f71306f1
PURNA1224 <sekhar1224@sasi.ac.in>
2023-08-14 17:02:42 +0530

    Age finder using Python

---
+++ ageFinder.py
@@ -0,0 +1,6 @@
+birth_year=input("Enter your birth year : ")
+print(type(birth_year))
+age=2022-int(birth_year)
+print(type(age))
+print(age)
+'''Here when we enter input from the terminal , that will be in string type whether we entered int value or string value or etc.'''
